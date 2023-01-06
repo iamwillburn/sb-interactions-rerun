b536992fa1cbac453d35f09617482fa25e2eaa59
Will Burn <wiburn@gmail.com>
2023-01-06 12:07:03 +0000

    initial commit

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Header } from './Header';
+import { Header } from "./Header";
+import { userEvent, within } from "@storybook/testing-library";
 
 export default {
-  title: 'Example/Header',
+  title: "Example/Header",
   component: Header,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 } as ComponentMeta<typeof Header>;
 
@@ -17,9 +18,15 @@ const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
   user: {
-    name: 'Jane Doe',
+    name: "Jane Doe",
   },
 };
 
 export const LoggedOut = Template.bind({});
 LoggedOut.args = {};
+
+LoggedOut.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const loginButton = await canvas.getByRole("button", { name: /Log in/i });
+  await userEvent.click(loginButton);
+};
